3c1a7f62cc373f99bc57a7cd3ed3aa98c9c921f6
caifeiheng <15139976537@163.com>
2023-05-24 10:58:59 +0800

    按需引入Element plus

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
+import {Check} from '@element-plus/icons-vue';
 
 </script>
 
 <template>
   <h1>874651</h1>
+  <el-button type='success' :icon="Check"></el-button>
 </template>
 
 <style scoped lang="less">
