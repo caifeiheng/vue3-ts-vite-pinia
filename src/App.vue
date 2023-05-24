464c9a2e08e7eee13bbc808373a92513c786e6b7
caifeiheng <15139976537@163.com>
2023-05-24 10:40:48 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <h1>874651</h1>
+</template>
+
+<style scoped lang="less">
+@color:green;
+h1{
+  color:@color;
+}
+</style>
+// @color : red;
